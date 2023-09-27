602b8d2efab037fb55b3920c1628e2adc6ed5404
ss1119 <y.sasaki111919@gmail.com>
2023-09-27 16:10:39 +0900

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      xs: { max: "280px" },
+      sm: "361px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+  },
+  plugins: [],
+};

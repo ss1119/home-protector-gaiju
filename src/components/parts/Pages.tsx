0c250df771168e4df69afddad83a1c3e40c2166e
ss1119 <y.sasaki111919@gmail.com>
2023-10-03 12:53:12 +0900

    fix

--- src/components/parts/Pages.tsx
+++ src/components/parts/Pages.tsx
@@ -29,35 +29,49 @@ export const Pages = () => {
         <img
           src="assets/pages/page4.webp"
           className="w-full h-auto lg:w-1/2"
-          alt="「天井や壁にシミがある」「動物の異臭がする」「天井から動物の足音が聞こえる」などの症状が出ていたら要注意です。"
+          alt="「天井や壁にシミがある」「配線コードや建物をかじった跡がある」などの症状が出ていたら要注意です。"
         />
       </div>
       <div className="flex justify-center">
         <img
           src="assets/pages/page5.webp"
           className="w-full h-auto lg:w-1/2"
-          alt="「庭や家の周りで動物の足跡を見た」「頻繁にダニやノミに刺される」などの被害があれば今すぐ弊社にお電話ください。"
+          alt="「家の周辺や室内で動物の足跡を見た」「頻繁にダニに刺される」などの被害があれば今すぐ弊社にお電話ください。"
         />
       </div>
       <div className="flex justify-center">
         <img
           src="assets/pages/page6.webp"
           className="w-full h-auto lg:w-1/2"
-          alt="被害を放置すると大変なことになります。"
+          alt="「室内で動物の異臭がする」「屋根裏から足音が聞こえる」などの被害があれば今すぐ弊社にお電話ください。"
+        />
+      </div>
+      <div className="flex justify-center">
+        <img
+          src="assets/pages/next.webp"
+          className="w-full h-auto lg:w-1/2"
+          alt="これらの症状を放置すると大変なことになります。"
         />
       </div>
       <div className="flex justify-center">
         <img
           src="assets/pages/page7.webp"
           className="w-full h-auto lg:w-1/2"
-          alt="最悪の場合、害獣被害によりあなたの大切なお家の不動産価値が0になる可能性があります。"
+          alt="「屋根裏に溜まった害獣の糞により天井が落ちる」「エボラ出血熱を発症してしまう」などの被害が出ます。"
         />
       </div>
       <div className="flex justify-center">
         <img
           src="assets/pages/page8.webp"
           className="w-full h-auto lg:w-1/2"
-          alt="そうなる前に弊社にお電話をください。"
+          alt="「配線をかじられ漏電し、火事になる」「害獣による騒音で不眠症になる」などの被害が出ます。"
+        />
+      </div>
+      <div className="flex justify-center">
+        <img
+          src="assets/pages/page9.webp"
+          className="w-full h-auto lg:w-1/2"
+          alt="「害獣に付着するダニやノミによりアレルギー感染症になる」などの被害が出ます。"
         />
       </div>
     </div>

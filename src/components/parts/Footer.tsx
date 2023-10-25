7ec2a5680a094c8be2ecd10c80de63471ed81cb8
ss1119 <y.sasaki111919@gmail.com>
2023-10-25 17:37:07 +0900

    フッター修正

--- src/components/parts/Footer.tsx
+++ src/components/parts/Footer.tsx
@@ -4,64 +4,41 @@ export const Footer = () => {
   const hour = getHour();
   const dayOfWeek = getDayOfWeek();
   return (
-    <div className="sticky bottom-0 z-50 lg:hidden">
-      <div className="flex-row items-center relative p-1 pb-2 w-full h-auto bg-orange-400 xs:pb-1 md:py-2">
-        <div className="flex">
-          <div className="flex justify-center w-3/5">
+    <div className="sticky footer-bg bottom-0 z-50 lg:hidden">
+      <div className="flex h-full">
+        <div className="flex items-end justify-center w-line pb-1">
+          <a href="https://lin.ee/mqP8zXC">
             <img
-              src="assets/footer/item1.webp"
-              alt="ご依頼受付中"
-              className="w-32 h-auto mr-3 mb-1 xs:w-16 md:w-60"
+              src="assets/footer/line.webp"
+              alt="簡単見積りはこちらから"
+              className="w-24 h-auto xs:w-20 md:w-52"
+              width={400}
+              height={165}
             />
-          </div>
-          <div className="flex justify-center w-2/5">
-            <img
-              src="assets/footer/item2.webp"
-              alt="現地調査無料"
-              className="w-32 h-auto mb-item2 xs:w-16 md:w-60"
-            />
-          </div>
+          </a>
         </div>
-        <div className="flex items-center justify-between mt-1">
+        <div className="flex items-end justify-center w-call pb-0.5">
           {9 <= hour && hour < 19 && dayOfWeek !== 6 ? (
-            <a
-              href="tel:0668857656"
-              className="flex items-center justify-center w-7/12 h-14 mr-3 xs:h-10 md:mr-0 md:h-28"
-            >
+            <a href="tel:0668857656">
               <img
-                src="assets/footer/call.webp"
+                src="assets/footer/call1.webp"
                 alt="お電話はこちらから"
-                className="w-56 h-auto md:w-10/12"
+                className="w-38 h-auto xs:w-28 md:w-72"
                 width={800}
                 height={237}
               />
             </a>
           ) : (
-            <a
-              href="tel:09060658318"
-              className="flex items-center justify-center w-7/12 h-14 mr-3 xs:h-10 md:mr-0 md:h-28"
-            >
+            <a href="tel:09060658318">
               <img
-                src="assets/footer/call.webp"
+                src="assets/footer/call2.webp"
                 alt="お電話はこちらから"
-                className="w-56 h-auto md:w-10/12"
+                className="w-38 h-auto xs:w-28 md:w-72"
                 width={800}
                 height={237}
               />
             </a>
           )}
-          <a
-            href="https://lin.ee/mqP8zXC"
-            className="flex flex-col justify-center w-5/12 h-14 xs:h-10 md:h-28 md:items-center"
-          >
-            <img
-              src="assets/footer/estimate.webp"
-              alt="簡単見積りはこちらから"
-              className="w-40 h-auto md:w-10/12"
-              width={400}
-              height={165}
-            />
-          </a>
         </div>
       </div>
     </div>
